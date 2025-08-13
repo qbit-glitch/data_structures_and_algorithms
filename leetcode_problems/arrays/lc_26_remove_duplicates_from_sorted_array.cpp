a85b187c079a0f4e10a566b0b99bc0cc7a8e57bc
qbit-glitch <brijbiharisingh1971@outlook.com>
2025-08-13 20:06:13 +0000

    remove duplicates from sorted array

---
+++ leetcode_problems/arrays/lc_26_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,102 @@
+/** Leetcode-26: Remove Duplicates From Sorted Array
+ * 
+Given an integer array `nums` sorted in **non-decreasing order**, remove the duplicates [**in-place**](https://en.wikipedia.org/wiki/In-place_algorithm) such that each unique element appears only **once**. The **relative order** of the elements should be kept the **same**. Then return *the number of unique elements in* `nums`.
+
+Consider the number of unique elements of `nums` to be `k`, to get accepted, you need to do the following things:
+
+- Change the array `nums` such that the first `k` elements of `nums` contain the unique elements in the order they were present in `nums` initially. The remaining elements of `nums` are not important as well as the size of `nums`.
+- Return `k`.
+
+**Custom Judge:**
+
+The judge will test your solution with the following code:
+
+```
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+```
+
+If all assertions pass, then your solution will be **accepted**.
+
+**Example 1:**
+
+```
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+```
+
+**Example 2:**
+
+```
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+```
+
+**Constraints:**
+
+- `1 <= nums.length <= 3 * 104`
+- `100 <= nums[i] <= 100`
+- `nums` is sorted in **non-decreasing** order.
+
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b; 
+    b = temp;
+}
+
+
+int removeDuplicates(vector<int> &a){
+    if (a.size() == 0) {
+        return 0;
+    }
+    if(a.size() == 1){
+        return 1;
+    }
+
+    int k = 0, item = a[0];
+    for(int i=1; i<a.size(); i++){
+        if( a[i] == item)
+            continue;
+        else{
+            k++;
+            item = a[i];
+            swap(a[i], a[k]);
+        }
+    } 
+    return k;
+
+}
+
+void printArray(vector<int> &a){
+    for(auto &i: a) {
+        cout <<  i << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> a{0,0,1,1,2,3,3,3};
+    int k = removeDuplicates(a);
+    cout <<  (k+1) << endl;
+    printArray(a);
+}
