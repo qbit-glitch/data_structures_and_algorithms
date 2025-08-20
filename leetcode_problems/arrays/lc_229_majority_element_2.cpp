1953c7a0c1a9c186450beb1c6b8e917fee8d3b41
qbit-glitch <brijbiharisingh1971@outlook.com>
2025-08-20 19:10:25 +0000

    leetcode-229: Majority Element II (Optimal)

--- leetcode_problems/arrays/lc_229_majority_element_2.cpp
+++ leetcode_problems/arrays/lc_229_majority_element_2.cpp
@@ -8,14 +8,46 @@ using namespace std;
 vector<int> majorityElement(vector<int> &a){
     vector<int> ans;
     map<int, int> hashMap;
+    int min = floor(a.size()/3) + 1;
+    
     for(auto &i: a){
         hashMap[i]++;
-    }
-    for(auto i: hashMap){
-        if(i.second > a.size()/3)
-            ans.push_back(i.first);
+        if(hashMap[i] == min)
+            ans.push_back(i);
     }
     return ans;
+}
+
+vector<int> majorityElement_Optimal(vector<int>& a) {
+    int cnt1=0, cnt2=0, el1=INT_MIN, el2=INT_MIN;
+    for(auto &i: a){
+        if(cnt1 == 0 and el2 != i){
+            cnt1 = 1; el1 = i;
+        }
+        else if(cnt2 == 0 and el1 != i){
+            cnt2 = 1; el2 = i;
+        }
+        else if(i == el1)   
+            cnt1++;
+        else if (i == el2)  
+            cnt2++;
+        else{
+            cnt1--; cnt2--;
+        }
+    }
+    cnt1=0, cnt2=0;
+    vector<int> ls;
+    for(auto &i: a){
+        if(el1 == i)
+            cnt1++;
+        if (el2 == i)
+            cnt2++;
+    }
+    if(cnt1 >= floor(a.size()/3)+1)
+        ls.push_back(el1);
+    if(cnt2 >= floor(a.size()/3)+1)
+        ls.push_back(el2);
+    return ls;
 }
 
 void printArray(vector<int> &a){
@@ -25,7 +57,7 @@ void printArray(vector<int> &a){
 }
 
 int main(){
-    vector<int> a{3,2,3};
-    vector<int> ans = majorityElement(a);
+    vector<int> a{1,1,1,1,3,3,2,3};
+    vector<int> ans = majorityElement_Optimal(a);
     printArray(ans);
 }
