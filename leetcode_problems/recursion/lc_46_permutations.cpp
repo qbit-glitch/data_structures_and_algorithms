045650cb579d3c64b1bcd6790a93ec5a6fdd860c
Umesh Kumar Singh <brijbiharisingh1971@outlook.com>
2025-10-11 20:30:07 +0000

    leetcode-46: permutations optimal solution

--- leetcode_problems/recursion/lc_46_permutations.cpp
+++ leetcode_problems/recursion/lc_46_permutations.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 
+/*-------------------- APPROACH 1 : using external map/array --------------*/
+
 void func(vector<int> &ds, vector<vector<int>> &result, vector<int> &nums, unordered_map<int, int> mpp){
     if(ds.size() == nums.size()){
         result.push_back(ds);
@@ -37,6 +39,40 @@ vector<vector<int>> permute(vector<int>& nums) {
     return result;
 }
 
+/* ------------------------ APPROACH I - ends --------------------------------- */
+
+
+
+
+/*--------- APPROACH II - using swapping and pointer to track indexes ---------*/
+
+void funcRecur_Optimal(int ind, vector<int> &nums, vector<vector<int>> &result){
+    if(ind == nums.size())
+    {   
+        result.push_back(nums);
+        return;
+    }
+
+    for(int i=ind; i<nums.size(); i++){
+        swap(nums[ind], nums[i]);
+        funcRecur_Optimal(ind+1, nums, result);
+        swap(nums[ind], nums[i]);
+    }
+}
+
+
+
+vector<vector<int>> permute_Optimal(vector<int>& nums) {
+    vector<vector<int>> result; 
+    vector<int> ds;
+    
+    funcRecur_Optimal(0, nums, result);
+    return result;
+}
+
+/* --------------------- APPROACH-II - ends -----------------------------------*/
+
+
 
 void printVector(vector<vector<int>> &a){
     for(auto &i: a){
@@ -49,6 +85,6 @@ void printVector(vector<vector<int>> &a){
 
 int main(){
     vector<int> nums {1,2,3};
-    vector<vector<int>> result = permute(nums);
+    vector<vector<int>> result = permute_Optimal(nums);
     printVector(result);
 }
