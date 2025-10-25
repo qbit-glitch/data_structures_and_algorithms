6db501542b568cae0a019cbf705d388b3d822130
Umesh Kumar Singh <brijbiharisingh1971@outlook.com>
2025-10-25 23:57:49 +0000

    lfu: add code for implementing lru for each frequency

--- leetcode_problems/stacks_aand_queues/lc_460_lfu_cache.cpp
+++ leetcode_problems/stacks_aand_queues/lc_460_lfu_cache.cpp
@@ -19,67 +19,127 @@ struct Node{
     }
 };
 
-void deleteNode(Node* node){
-    Node* nextNode = node->next;
-    Node* prevNode = node->prev;
-
-    nextNode->prev = prevNode;
-    prevNode->next = nextNode;
-}
-
-void insertAfterHead(Node* node, Node* head){
-    Node* nodeAfterHead = head->next;
-    head->next = node;
-    node->prev = head;
-
-    node->next = nodeAfterHead;
-    nodeAfterHead->prev = node;
-}
-
-
-class LFUCache {
-
+class LRUCache {
 private:
-    unordered_map<int, pair<Node*, int>> mpp;
+    int capacity;
     Node* head = new Node(-1,-1);
     Node* tail = new Node(-1,-1);
-    int capacity;
+    unordered_map<int, Node*> mpp;
 
 public:
-    LFUCache(int capacity1) {
+    LRUCache(int capacity1) {
         capacity = capacity1;
         mpp.clear();
         head->next = tail;
         tail->prev = head;
     }
+
     
+    void deleteNode(Node* node){
+        Node* nextNode = node->next;
+        Node* prevNode = node->prev;
+
+        nextNode->prev = prevNode;
+        prevNode->next = nextNode;
+    }
+
+    void insertAfterHead(Node* node){
+        Node* nodeAfterHead = head->next;
+        head->next = node;
+        node->prev = head;
+
+        node->next = nodeAfterHead;
+        nodeAfterHead->prev = node;
+    }
+
     int get(int key) {
         if(mpp.find(key) == mpp.end())
             return -1;
-
-        pair<Node*, int> nodeWithCount = mpp[key];
-        Node* node = nodeWithCount.first;
+        Node* node = mpp[key];
         
         deleteNode(node);
-        insertAfterHead(node, head);
-
-        nodeWithCount.second++;
-
-        return nodeWithCount.first->val;
+        insertAfterHead(node);
+        
+        return node->val;
     }
     
     void put(int key, int value) {
         if(mpp.find(key) != mpp.end()){
-            pair<Node*, int> nodeWithCount = mpp[key];
-            
-            deleteNode(nodeWithCount.first);
-            nodeWithCount.first->val = value;
-            insertAfterHead(nodeWithCount.first, head);
+            Node* node = mpp[key];
+            node->val = value;
 
-            nodeWithCount.second++;
+            deleteNode(node);
+            insertAfterHead(node);
         }
         else {
+            if(mpp.size() == capacity){
+                Node* temp = tail->prev;
+                mpp.erase(temp->key);
+                
+                deleteNode(temp);
+                delete temp;
+
+            }
+
+            Node* node = new Node(key, value);
+            mpp[key] = node;
+            insertAfterHead(node);
             
+        }
+        
+    }
+
+    void printCache(){
+        Node* temp = head;
+        while(temp != nullptr){
+            cout << temp->key << "," << temp->val << "   ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+class LFUCache {
+private:
+    unordered_map<int, LRUCache> freqListCache;
+    unordered_map<int, Node*> keyNode;
+    unordered_map<Node*, int> freqCnt;
+    int minFreq, capacity;
+
+public:
+    LFUCache(int capacity1) {
+        capacity = capacity1;
+        freqListCache.clear();
+        keyNode.clear();        
+    }
+    
+    int get(int key) {
+        if(keyNode.find(key) == keyNode.end())
+            return -1;
+        Node* node = keyNode[key];
+        int freq = freqCnt[node];
+        
+        freqListCache[freq].deleteNode(node);
+        
+        freq++;
+        freqListCache[freq].insertAfterHead(node);
+        freqCnt[node] = freq;
+
+        return node->val;
+    }
+    
+    void put(int key, int value) {
+        if(keyNode.find(key) != keyNode.end()){
+            Node* node = keyNode[key];
+            node->val = value;
+
+            freqListCache[freqCnt[node]++].deleteNode(node);
+            freqListCache[freqCnt[node]].insertAfterHead(node);            
+        }
+        else {
+            if(keyNode.size() == capacity){
+                
+            }
         }
     }
 };
